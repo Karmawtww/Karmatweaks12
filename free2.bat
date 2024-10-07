ebb93b6c1a8c726f4afcb7be271424013670a7ae
Karmawtww <trentonmasterson3@gmail.com>
2024-10-07 01:39:39 -0400

    Create free2.bat

---
+++ free2.bat
@@ -0,0 +1,63 @@
+@echo off
+chcp 65001 >nul
+title KARMA OP TWEAKS
+color 3
+
+:banner
+echo.
+echo ██ ▄█▀▄▄▄       ██▀███   ███▄ ▄███▓ ▄▄▄         ▄▄▄█████▓ █     █░▓█████ ▄▄▄       ██ ▄█▀  ██████ 
+echo ██▄█▒▒████▄    ▓██ ▒ ██▒▓██▒▀█▀ ██▒▒████▄       ▓  ██▒ ▓▒▓█░ █ ░█░▓█   ▀▒████▄     ██▄█▒ ▒██    ▒ 
+echo ▓███▄░▒██  ▀█▄  ▓██ ░▄█ ▒▓██    ▓██░▒██  ▀█▄     ▒ ▓██░ ▒░▒█░ █ ░█ ▒███  ▒██  ▀█▄  ▓███▄░ ░ ▓██▄   
+echo ▓██ █▄░██▄▄▄▄██ ▒██▀▀█▄  ▒██    ▒██ ░██▄▄▄▄██    ░ ▓██▓ ░ ░█░ █ ░█ ▒▓█  ▄░██▄▄▄▄██ ▓██ █▄   ▒   ██▒
+echo ▒██▒ █▄▓█   ▓██▒░██▓ ▒██▒▒██▒   ░██▒ ▓█   ▓██▒     ▒██▒ ░ ░░██▒██▓ ░▒████▒▓█   ▓██▒▒██▒ █▄▒██████▒▒
+echo ▒ ▒▒ ▓▒▒▒   ▓▒█░░ ▒▓ ░▒▓░░ ▒░   ░  ░ ▒▒   ▓▒█░     ▒ ░░   ░ ▓░▒ ▒  ░░ ▒░ ░▒▒   ▓▒█░▒ ▒▒ ▓▒▒ ▒▓▒ ▒ ░
+echo ░ ░▒ ▒░ ▒   ▒▒ ░  ░▒ ░ ▒░░  ░      ░  ▒   ▒▒ ░       ░      ▒ ░ ░   ░ ░  ░ ▒   ▒▒ ░░ ░▒ ▒░░ ░▒  ░ ░
+echo ░ ░░ ░  ░   ▒     ░░   ░ ░      ░     ░   ▒        ░        ░   ░     ░    ░   ▒   ░ ░░ ░ ░  ░  ░  
+echo ░  ░        ░  ░   ░            ░         ░  ░                ░       ░  ░     ░  ░░  ░         ░ 
+timeout 3 >nul
+echo ...
+timeout 3 >nul
+echo ...
+timeout 3 >nul
+echo ...
+timeout 3 >nul
+echo ...
+timeout 3 >nul
+cls 
+timeout 3 >nul
+echo The Tweaks Have Succesfully Started!
+timeout 3 >nul
+echo ...
+timeout 3 >nul
+echo Waiting on less latency feature...
+timeout 3 >nul
+echo ...
+timeout 3 >nul
+echo Success!
+timeout 3 >nul
+echo ...
+timeout 3 >nul
+echo Adding all other latency remover's...
+timeout 3 >nul
+echo ...
+timeout 3 >nul
+echo Added!
+timeout 3 >nul
+echo ...
+timeout 3 >nul
+echo Optimizing network usage...
+timeout 3 >nul
+echo ...
+timeout 3 >nul
+echo Success!
+timeout 3 >nul
+echo ...
+echo Your PC Is Finished!
+timeout 3 >nul
+echo ...
+timeout 3 >nul
+echo ...
+timeout 3 >nul
+echo Press any key to close the tweaking software.
+pause >nul
+exit
